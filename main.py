9b217fcd7c75b5377424185653b44140a0ee30a8
Daniel Jug <daniel.jug@tvz.hr>
2023-04-02 12:50:48 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#Ova linija mora biti komentar!
+print('Dobrodošli', '!'*3)
+# Napisati sljedeće izraze u jednoj liniji koda
+ime = input('Unesite vase ime:')
+prezime=input('Unesite vase prezime:')
+JMBAG=input('Unesite vas JMBAG:')
+# TODO zatražite od korisnika broj godina
+godine=input('Unesite vas broj godina:')
+# TODO ime i prezime ispišite u navodnim znakovima
+print("Korisnik : '", ime, "' '", prezime, "'", godine, end='!')
+
+
+
